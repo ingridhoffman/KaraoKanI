25beb613d50f5a7b1b64d67202b1651aded75b1b
trapezechic <ingridhoffman@me.com>
2020-03-03 18:07:36 -0800

    script work, see notes.md

--- script.js
+++ script.js
@@ -8,6 +8,55 @@ var song = "";
 var encodedSong = encodeURI(song);
 var resultsNUM = 3;
 
+// Function to show the results from MusixMatch
+function displayMusicMatch(response) {
+	// clear previous song list
+	$("#songList").html('<p class="panel-heading">Song Versions:</p>');
+	// temp button for troubleshooting
+	// loop through new results
+	var countMM;
+	for (countMM = 0; countMM < resultsNUM; countMM++) {
+		//Assigns song from MusicMatch
+		var songFromMM = JSON.parse(response).message.body.track_list[countMM].track
+			.track_name;
+		console.log("Song from MM: " + songFromMM);
+		//Assigns artist from MusicMatch
+		var artistFromMM = JSON.parse(response).message.body.track_list[countMM]
+			.track.artist_name;
+		console.log("Artist from MM: " + artistFromMM);
+		// Log the SHARE lyrics URL for first result
+		var idFromMM = JSON.parse(response).message.body.track_list[countMM].track
+			.track_id;
+		console.log("ID from MM: " + idFromMM);
+		var newSongBtn =
+			'<a class="songBtn panel-block" id="' +
+			idFromMM +
+			'"><p id="songName">' +
+			songFromMM +
+			'</p><p id="artistName">' +
+			artistFromMM +
+			"</p></a>";
+		$("#songList").append(newSongBtn);
+	}
+	// Song button function (assigned to class songBtn) to listen for user selection
+	$(".songBtn").click(function(event) {
+		event.preventDefault();
+		console.log("Song button selected!");
+		// SongName on selected button
+		var selectedSong = $(this)
+			.children("#songName")
+			.text();
+		// ArtistName on selected button
+		var selectedArtist = $(this)
+			.children("#artistName")
+			.text();
+		getDeezer(selectedSong, selectedArtist);
+		// ID from selected button for Lyrics call
+		var selectedID = this.id;
+		showLyrics(selectedID);
+	});
+}
+
 // AJAX call for song information, specifically via ?q_track variable
 // The URL we need to query song title (q_track). Note that you have to add the cors-anywhere element, see below sample
 function getMusicMatch(encodedSong) {
@@ -53,85 +102,58 @@ function getMusicMatch(encodedSong) {
 }
 
 //Search button function (assigned to ID #searchBtn currently), turns text input into song
-
 $("#searchBtn").click(function(event) {
 	event.preventDefault();
-	var song = $("#searchBar").val();
-	console.log(song);
-	var encodedSong = encodeURI(song);
+	song = $("#searchBar").val();
+	console.log("User enterd: " + song);
+	encodedSong = encodeURI(song);
 	getMusicMatch(encodedSong);
 });
 
-// Temporary song button function - will eventually be the function to show the results from Music Match
-function displayMusicMatch(response) {
-	// temp assignments
-	$("#songList").empty();
-	var countMM;
-	for (countMM = 0; countMM < resultsNUM; countMM++) {
-		//Assigns song from MusicMatch
-		var songFromMM = JSON.parse(response).message.body.track_list[countMM].track
-			.track_name;
-		console.log(songFromMM);
-		//Assigns artist from MusicMatch
-		var artistFromMM = JSON.parse(response).message.body.track_list[countMM]
-			.track.artist_name;
-		console.log(artistFromMM);
-		// eventually put in loop for all returned elements
-		// Log the SHARE lyrics URL for first result
-		var idFromMM = JSON.parse(response).message.body.track_list[countMM].track
-			.track_id;
-		console.log(idFromMM);
-		var newSongBtn =
-			'<a class="songBtn panel-block" id="' +
-			idFromMM +
-			'"><p id="songName">' +
-			songFromMM +
-			'</p><p id="artistName">' +
-			artistFromMM +
-			"</p></a>";
-		$("#songList").append(newSongBtn);
-	}
+//Creating showLyrics function that triggers off song ID from musixmatch, to generate lyrics.
+function showLyrics(ID) {
+	console.log("ID of selected song: " + ID);
+	// var queryURLMM =
+	// 	"https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=" +
+	// 	encodedSong +
+	// 	"&page_size=" +
+	// 	resultsNUM +
+	// 	"&page=1&s_track_rating=desc&apikey=" +
+	// 	musixMatchAPIKey;
 
-	//Creating showLyrics function that triggers off song ID from musixmatch, to generate lyrics.
-	function showLyrics() {
-		var queryURLMM =
-			"https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=" +
-			encodedSong +
-			"&page_size=" +
-			resultsNUM +
-			"&page=1&s_track_rating=desc&apikey=" +
-			musixMatchAPIKey;
-
-		//Query and console logging the object below
-		$.ajax({
-			url: queryURLMM,
-			method: "GET"
-		}).then(function(response) {});
-	}
-}
-
-function displayDeezer(result) {
-	console.log(result);
-	var artistShowing = result.data[0].artist.name;
-	var imageURL = result.data[0].artist.picture_big;
-	var playSample = result.data[0].preview;
-
-	var artistInfoBlock =
-		'<h3 id="artistName">' +
-		artistShowing +
-		'</h3><img src="' +
-		imageURL +
-		'" alt="Artist Image">';
-	$("#artistBox").html(artistInfoBlock);
-
-	var playBox =
-		'<div id="playBox"><h3>Play Sample:</h3><audio controls><source src="' +
-		playSample +
-		'" type="audio/mpeg"></audio></div>';
-	// where to put this??
+	//Query and console logging the object below
+	// $.ajax({
+	// 	url: queryURLMM,
+	// 	method: "GET"
+	// }).then(function(response) {});
 }
 
 // DEEZER INTERACTION
+// Function to display results from Deezer
+function displayDeezer(result) {
+	var songArtist = result.data[0].artist.name;
+	var songAlbum = result.data[0].album.title;
+	var imageURL = result.data[0].album.cover_big;
+	var playSample = result.data[0].preview;
+	// display infomration about the artist for the selected song
+	var infoBlock =
+		"<h3>" +
+		songArtist +
+		"</h3><h2>" +
+		songAlbum +
+		'</h2><img src="' +
+		imageURL +
+		'" alt="Album Cover">';
+	$("#artistBox").html(infoBlock);
+	// display audio control to play sample of selected song
+	var playControl =
+		'<h2>Play Sample:</h2><audio controls><source src="' +
+		playSample +
+		'" type="audio/mpeg"></audio>';
+	$("#playBox").html(playControl);
+}
+
+// AJAX call to Deezer using information from button user selected
 function getDeezer(song, artist) {
 	console.log("Ask Deezer: " + song);
 	console.log("Ask Deezer: " + artist);
@@ -154,20 +176,3 @@ function getDeezer(song, artist) {
 		displayDeezer(response);
 	});
 }
-
-// Song button function (assigned to class songBtn)
-$("a").click(function(event) {
-	console.log("Deezer called");
-	event.preventDefault();
-
-	// SongName on selected button (or should we just use the global song variable?)
-	var selectedSong = $(this)
-		.children("#songName")
-		.text();
-
-	// ArtistName on selected button (or should we just use the global song variable?)
-	var selectedArtist = $(this)
-		.children("#artistName")
-		.text();
-	getDeezer(selectedSong, selectedArtist);
-});
